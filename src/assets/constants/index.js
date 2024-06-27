3469ff3bf5d34aff8a41b3d4f11f4ebd80745175
muktadirhossain <muktadirhossainrabbi@gmial.com>
2024-06-27 20:55:57 +0600

    baseURL edited

--- src/assets/constants/index.js
+++ src/assets/constants/index.js
@@ -1,13 +1,14 @@
 const CONSTANTS = {
     // baseUrl: 'http://localhost:3000',
-    baseUrl: process.env.BASE_URL.toString(),
+    
+    baseUrl: process.env.BASE_URL?.toString(),
     cookieName: 'easy-table-auth',
     tokenSecret: 'tokenSecret123456789',
     CURRENCY: '€',
     // MONGO_CONNECTION_STRING: 'mongodb://127.0.0.1:27017/ets',
     // DB_NAME: 'ets',
     DB_NAME: "easyTableService",
-    MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING.toString(),
+    MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING?.toString(),
 }
 
 export default CONSTANTS
